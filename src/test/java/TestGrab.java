454611ae2a70c7b905541a6df55151e40b3fec57
john lim <johnlim@johns-MacBook-Pro-2.local>
2019-04-29 08:00:43 +0800

    test grab

--- src/test/java/TestGrab.java
+++ src/test/java/TestGrab.java
@@ -121,31 +121,10 @@ class TestGrab {
 
     }
 
-    public int solution(String E, String L) {
-        // write your code in Java SE 8
-        String[] t1= E.split(":");
-        String[] t2= L.split(":");
-        int hh1= Integer.parseInt(t1[0]);
-        int mm1 =Integer.parseInt(t1[1]);
 
-        int hh2= Integer.parseInt(t2[0]);
-        int mm2 =Integer.parseInt(t2[1]);
-
-        int hh = hh2-hh1;
-        int mm = mm2-mm1;
-        if(mm >0) hh+=1;
-        int cost=2;
-        if(hh>=1) cost+=3;
-        cost+=(hh-1)*4;
-        return cost;
-    }
 
     public static void main(String args[]){
-
         TestGrab s= new TestGrab();
-        //System.out.println(s.solution("10:00","13:21"));
-
-
         //System.out.println(s.solution(new int[]{1,2,4,5,7,29,30}));
         System.out.println(s.solutionp(new int[]{1,2,4,5,7,29,30}));
         //System.out.println(s.solutionp(new int[]{1,4,7,10,11,14,29,30}));
---
+++ src/test/java/time/TimeString.java
@@ -0,0 +1,28 @@
+package time;
+
+public class TimeString {
+    // From grab
+    public int solution(String E, String L) {
+        // write your code in Java SE 8
+        String[] t1 = E.split(":");
+        String[] t2 = L.split(":");
+        int hh1 = Integer.parseInt(t1[0]);
+        int mm1 = Integer.parseInt(t1[1]);
+
+        int hh2 = Integer.parseInt(t2[0]);
+        int mm2 = Integer.parseInt(t2[1]);
+
+        int hh = hh2 - hh1;
+        int mm = mm2 - mm1;
+        if (mm > 0) hh += 1;
+        int cost = 2;
+        if (hh >= 1) cost += 3;
+        cost += (hh - 1) * 4;
+        return cost;
+    }
+
+    public static void main(String[] args) {
+        TimeString s = new TimeString();
+        System.out.println(s.solution("10:00", "13:21"));
+    }
+}
