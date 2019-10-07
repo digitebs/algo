fac29a628bb6ca538c0abdfc89639aa745c6df69
iody <ebs_cbn@yahoo.com>
2019-10-07 18:05:25 +0800

    Update ThreeSumsArray.java

--- src/test/java/array/ThreeSumsArray.java
+++ src/test/java/array/ThreeSumsArray.java
@@ -24,6 +24,24 @@ public class ThreeSumsArray {
         }
     }
 
+    void sum(int[] arr, int target){
+        Arrays.sort(arr);// nlogn
+        for(int i = 0; i < arr.length; i++){ // n * n == n^2
+            int j = i+1, k = arr.length -1;
+            while(j < k){ // collapse it in
+                int sum = arr[i] + arr[j]+ arr[k];
+                if(sum  > target) k--; // we move backward
+                else if(sum < target) j++;
+                else if(sum == target) {
+                    System.out.printf("%d,%d,%d\n", i, j, k);
+                    i++;
+                    k--;
+                    //break;
+                }
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
            new ThreeSumsArray().sum(new int[]{2, 7, 11, 15}, 24);
