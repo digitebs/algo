1df1c3a041ccc0b8e9d6af82a2b96b6d7e307ecb
Lim, John <john.lim@rakuten.com>
2019-10-30 18:02:13 +0800

    cleaning up

--- src/test/java/math/ProductExceptSelf.java
+++ src/test/java/math/ProductExceptSelf.java
@@ -1,16 +1,24 @@
 package math;
 
+import java.util.Arrays;
+
 public class ProductExceptSelf {
-    public int[] productExceptSelf(int[] num){
+    static int[] productExceptSelf(int[] num) {
         // 1 2 3 4
         // (2 * 3 * 4)
         // (1) * (3 * 4)
         // (1 * 2) * (4)
         // (1 * 2 * 3)
         int[] dp = new int[num.length];
-        dp[0]=1;
-        for(int i = 1, tmp=1; i < num.length;i++) dp[i]=tmp *= num[i-1];
-        for(int i = num.length-2,tmp=1; i >= 0;i--) dp[i]*=tmp *= num[i+1];
+        dp[0] = 1;
+        for (int i = 1, tmp = 1; i < num.length; i++) dp[i] = tmp *= num[i - 1];
+        for (int i = num.length - 2, tmp = 1; i >= 0; i--) dp[i] *= tmp *= num[i + 1];
         return dp;
+    }
+
+    public static void main(String[] args) {
+        int[] res = productExceptSelf(new int[]{1, 2, 3, 4});
+
+        System.out.println(Arrays.toString(res));
     }
 }
