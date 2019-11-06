5baae2789867de87003fe81e0ff9908f92652a2c
Lim, John <john.lim@rakuten.com>
2019-11-06 15:20:38 +0800

    using sliding window

--- src/test/java/map/LongestSubstringNoDup.java
+++ src/test/java/map/LongestSubstringNoDup.java
@@ -1,11 +1,13 @@
 package map;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class LongestSubstringNoDup {
     // using hashmap to store dup index
     // a b c d e f g h c a
-    static int longestsub(String str) {
+    static int longestsub2(String str) {
         int maxLen = 0;
         int start = -1;
         HashMap<Character,Integer> hm = new HashMap<>();
@@ -18,6 +20,22 @@ public class LongestSubstringNoDup {
         return maxLen;
     }
 
+    // a b c d b c a f
+    // sliding window, at least 2(n) due to backtrack removal
+    static int longestsub(String str) {
+        int res = 0,right=0,left=0;
+        Set<Character> hs = new HashSet<>();
+        while(right< str.length()){
+            if(hs.contains(str.charAt(right))){
+                hs.remove(str.charAt(left++));
+            }else{
+                hs.add(str.charAt(right++));
+                res = Math.max(hs.size() ,res);
+            }
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         System.out.println(longestsub("ABCDEFAGYTUIKGE"));
         System.out.println(longestsub("ABCDEFGE"));
@@ -25,5 +43,6 @@ public class LongestSubstringNoDup {
         System.out.println(longestsub("abcaacefgh"));
         System.out.println(longestsub("abcdacefgh"));
         System.out.println(longestsub("abdaeedfg"));
+        System.out.println(longestsub("ninenine"));
     }
 }
