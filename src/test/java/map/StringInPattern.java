0320034ab08a7f35cc66be017913fa7df73462d8
Lim, John <john.lim@rakuten.com>
2019-12-09 17:39:39 +0800

    adding isak solution

--- src/test/java/map/StringInPattern.java
+++ src/test/java/map/StringInPattern.java
@@ -1,6 +1,7 @@
 package map;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author john.lim
@@ -9,8 +10,7 @@ public class StringInPattern {
 
     // aaba
     // apple apple orange apple true
-    static boolean inPattern(String a, String s) {
-
+    static boolean isMatch2(String a, String s) {
         String[] words = s.split(" ");
         if (a.length() != words.length) return false;
 
@@ -37,8 +37,27 @@ public class StringInPattern {
         return result.equals(a);
     }
 
+    public static boolean isMatch(String pattern, String input) {
+        char[] ch = pattern.toCharArray();
+        String[] word = input.split(" ");
+        if (ch.length != word.length) {
+            return false;
+        }
+        Map<Character, String> map = new HashMap<>();
+        for (int i = 0; i < word.length; i++) {
+            if (!map.containsKey(ch[i])) {
+                map.put(ch[i], word[i]);
+            } else {
+                if (!map.get(ch[i]).equalsIgnoreCase(word[i])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
     public static void main(String args[]) {
-        System.out.println(inPattern("aaba", "apple apple orange apple"));
-        System.out.println(inPattern("aaba", "apple apple orange big"));
+        System.out.println(isMatch("aaba", "apple apple orange apple"));
+        System.out.println(isMatch("aaba", "apple apple orange big"));
     }
 }
