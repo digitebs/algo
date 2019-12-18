9d5364cc87745a5070dc527f149022b30fd6296c
Lim, John <john.lim@rakuten.com>
2019-12-18 17:22:52 +0800

    update solution set

--- src/test/java/array/ArrayRotate.java
+++ src/test/java/array/ArrayRotate.java
@@ -56,6 +56,23 @@ public class ArrayRotate {
 
     }
 
+    // rotate by reversal
+    public void rotateReverse(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
     public void rotate3(int[] A, int K){
         int[] r = new int[A.length];
         for(int i =0; i < A.length ; i++){
