9ec1ae2c15a972043893e549ea5cc7b115784fbb
Lim, John <john.lim@rakuten.com>
2020-01-28 16:57:21 +0800

    adding gcd of strings

---
+++ src/test/java/math/GCDStrings.java
@@ -0,0 +1,39 @@
+package math;
+
+/**
+ * @author john.lim
+ *
+ * you can find the gcd pf string by calculating the gcd of length of both string.
+ * and checking if all of the substring matches it
+ *
+ * instaead of brute force reduing the numbers and trying if it can fit.
+ *
+ * checking the length 1st whats the largets possible gcd. since its repetitive then its a factor
+ */
+public class GCDStrings {
+    public String gcdOfStrings(String str1, String str2) {
+        String longer = str1.length() > str2.length()? str1 : str2;
+        String shorter = str1.equals(longer) ? str2 : str1;
+        int gcd = calculateGCD(longer.length(), shorter.length());
+
+        int index = 0;
+        String sgcd = shorter.substring(0, gcd);
+        while(index < longer.length()){
+            int cur = longer.indexOf(sgcd, index);
+            if(cur != -1){
+                index = cur + gcd;
+            } else {
+                return "";
+            }
+        }
+        return longer.length() % shorter.length() == 0 ? shorter : sgcd;
+    }
+
+    private int calculateGCD(int a, int b){
+        return b == 0? a : calculateGCD(b, a % b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new GCDStrings().gcdOfStrings("ABABAB","ABAB"));
+    }
+}
--- src/test/java/string/RepeatedSubstringPattern.java
+++ src/test/java/string/RepeatedSubstringPattern.java
@@ -1,5 +1,7 @@
 package string;
 
+import java.util.Arrays;
+
 /**
  * Given a non-empty string check if it can be constructed by taking a substring of it and
  * appending multiple copies of the substring together.
@@ -28,7 +30,81 @@ public class RepeatedSubstringPattern {
         return false;
     }
 
+
+    /**
+     *
+     * lps using dp storing the  len in an array
+     */
+    static void computeLPSArray(String str, int M,
+                                int lps[])
+    {
+        // lenght of the previous
+        // longest prefix suffix
+        int len = 0;
+
+        int i;
+
+        lps[0] = 0; // lps[0] is always 0
+        i = 1;
+
+        // the loop calculates lps[i]
+        // for i = 1 to M-1
+        while (i < M)
+        {
+            System.out.println(i+" "+len +" "+ str.charAt(i)+ " "+ str.charAt(len)+" "+ Arrays.toString(lps));
+            if (str.charAt(i) == str.charAt(len))
+            {
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else // (pat[i] != pat[len])
+            {
+                if (len != 0)
+                {
+                    // This is tricky. Consider the
+                    // example AAACAAAA and i = 7.
+                    len = lps[len-1];
+
+                    // Also, note that we do
+                    // not increment i here
+                }
+                else // if (len == 0)
+                {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+    }
+
+    static boolean isRepeat(String str)
+    {
+        // Find length of string and create
+        // an array to store lps values used in KMP
+        int n = str.length();
+        int lps[] = new int[n];
+
+        // Preprocess the pattern (calculate lps[] array)
+        computeLPSArray(str, n, lps);
+
+        // Find length of longest suffix
+        // which is also prefix of str.
+        int len = lps[n-1];
+
+        // If there exist a suffix which is also
+        // prefix AND Length of the remaining substring
+        // divides total length, then str[0..n-len-1]
+        // is the substring that repeats n/(n-len)
+        // times (Readers can print substring and
+        // value of n/(n-len) for more clarity.
+
+        System.out.println(n +" "+len);
+        return (len > 0 && n%(n-len) == 0)? true: false;
+    }
+
     public static void main(String[] args) {
-        System.out.println(new RepeatedSubstringPattern().repeatedSubstringPattern("abab"));
+        System.out.println(new RepeatedSubstringPattern().isRepeat("EEEKSEEEKSEEEKS"));
+       // System.out.println(new RepeatedSubstringPattern().repeatedSubstringPattern("abab"));
     }
 }
