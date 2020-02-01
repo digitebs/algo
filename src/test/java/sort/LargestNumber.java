fae3113d5057e8a0ca0683865ec608eb6a1225a9
john lim <johnlim@johns-MacBook-Pro-2.local>
2020-02-01 22:01:55 +0800

    cleaning up

---
+++ src/test/java/sort/LargestNumber.java
@@ -0,0 +1,37 @@
+package sort;
+
+import java.util.*;
+
+public class LargestNumber {
+    static String largestNumber(final List<Integer> a) {
+        StringBuilder res= new StringBuilder();
+        ArrayList<Integer> b = new ArrayList<>(a);
+
+
+        Collections.sort(a, (o1, o2) -> {
+            String str1 = String.valueOf(o1);
+            String str2= String.valueOf(o2);
+            return (str1+str2).compareTo(str2+str1);
+        });
+
+        for (int i = a.size()-1; i >=0; i--) {
+            res.append(a.get(i));
+        }
+        if(res.toString().matches("0+")){
+            return new String("0");
+        }
+        return res.toString();
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(largestNumber(Arrays.asList(3, 305,340,0,98,9890, 3139, 5, 9)));
+        System.out.println(largestNumber(Arrays.asList( 472, 663, 964, 722, 485, 852, 635, 4, 368, 676, 319, 412)));
+
+        System.out.println(largestNumber(Arrays.asList( 12, 123)));
+        System.out.println(largestNumber(Arrays.asList( 12, 121)));
+        System.out.println(largestNumber(Arrays.asList( 8, 89)));
+        System.out.println(largestNumber(Arrays.asList( 3, 30)));
+        System.out.println(largestNumber(Arrays.asList(0, 0,0,0,1)));
+    }
+}
--- src/test/java/string/CrackCodeString.java
+++ src/test/java/string/CrackCodeString.java
@@ -224,63 +224,6 @@ public class CrackCodeString {
         return -1;
     }
 
-    @Test
-    public void largestNumber() {
-
-
-        String a ="99799599398980975969969943923911910884880866838814785722694692689684674664657623599563515496494470444438437426400399379377369365363359358341329324302302250231230204176168161152143126124121113106";
-        String b ="99799599398098975969969943923911910884880866838814785722694692689684674664657623599563515496494470444438437426400399379377369365363359358341329324302302250231230204176168161152143126124121113106";
-
-
-        for (int i = 0; i < a.length(); i++) {
-            if(a.charAt(i)!= b.charAt(i))
-                System.out.println("wwwww"+ i + " "+a.charAt(i));
-
-        }
-        System.out.println(largestNumber(Arrays.asList(3, 305,340,0,98,9890, 3139, 5, 9)));
-        System.out.println(largestNumber(Arrays.asList( 472, 663, 964, 722, 485, 852, 635, 4, 368, 676, 319, 412)));
-
-        System.out.println(largestNumber(Arrays.asList( 12, 123)));
-        System.out.println(largestNumber(Arrays.asList( 12, 121)));
-        System.out.println(largestNumber(Arrays.asList( 8, 89)));
-        System.out.println(largestNumber(Arrays.asList( 3, 30)));
-        System.out.println(largestNumber(Arrays.asList(0, 0,0,0,1)));
-    }
-
-    public String largestNumber(final List<Integer> a) {
-        StringBuilder res= new StringBuilder();
-        ArrayList<Integer> b = new ArrayList<>(a);
-
-
-        Collections.sort(a, new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                String str1 = String.valueOf(o1);
-                String str2= String.valueOf(o2);
-                return (str1+str2).compareTo(str2+str1);
-            }
-        });
-
-        for (int i = a.size()-1; i >=0; i--) {
-            res.append(a.get(i));
-        }
-        if(res.toString().matches("0+")){
-            return new String("0");
-        }
-        return res.toString();
-
-
-    }
-    @Test
-    public void firstMissingPositive() {
-
-
-        ArrayList a = new ArrayList(Arrays.asList(1,3));
-        a = new ArrayList(Arrays.asList(1,2,0));
-       // a = new ArrayList(Arrays.asList(3,4,-1,1));
-       // a = new ArrayList(Arrays.asList(-8, -7, -6));
-      //  a = new ArrayList(Arrays.asList(417, 929, 845, 462, 675, 175, 73, 867, 14, 201, 777, 407, 80, 882, 785, 563, 209, 261, 776, 362, 730, 74, 649, 465, 353, 801, 503, 154, 998, 286, 520, 692, 68, 805, 835, 210, 819, 341, 564, 215, 984, 643, 381, 793, 726, 213, 866, 706, 97, 538, 308, 797, 883, 59, 328, 743, 694, 607, 729, 821, 32, 672, 130, 13, 76, 724, 384, 444, 884, 192, 917, 75, 551, 96, 418, 840, 235, 433, 290, 954, 549, 950, 21, 711, 781, 132, 296, 44, 439, 164, 401, 505, 923, 136, 317, 548, 787, 224, 23, 185, 6, 350, 822, 457, 489, 133, 31, 830, 386, 671, 999, 255, 222, 944, 952, 637, 523, 494, 916, 95, 734, 908, 90, 541, 470, 941, 876, 264, 880, 761, 535, 738, 128, 772, 39, 553, 656, 603, 868, 292, 117, 966, 259, 619, 836, 818, 493, 592, 380, 500, 599, 839, 268, 67, 591, 126, 773, 635, 800, 842, 536, 668, 896, 260, 664, 506, 280, 435, 618, 398, 533, 647, 373, 713, 745, 478, 129, 844, 640, 886, 972, 62, 636, 79, 600, 263, 52, 719, 665, 376, 351, 623, 276, 66, 316, 813, 663, 831, 160, 237, 567, 928, 543, 508, 638, 487, 234, 997, 307, 480, 620, 890, 216, 147, 271, 989, 872, 994, 488, 291, 331, 8, 769, 481, 924, 166, 89, 824, -4, 590, 416, 17, 814, 728, 18, 673, 662, 410, 727, 667, 631, 660, 625, 683, 33, 436, 930, 91, 141, 948, 138, 113, 253, 56, 432, 744, 302, 211, 262, 968, 945, 396, 240, 594, 684, 958, 343, 879, 155, 395, 288, 550, 482, 557, 826, 598, 795, 914, 892, 690, 964, 981, 150, 179, 515, 205, 265, 823, 799, 190, 236, 24, 498, 229, 420, 753, 936, 191, 366, 935, 434, 311, 920, 167, 817, 220, 219, 741, -2, 674, 330, 909, 162, 443, 412, 974, 294, 864, 971, 760, 225, 681, 689, 608, 931, 427, 687, 466, 894, 303, 390, 242, 339, 252, 20, 218, 499, 232, 184, 490, 4, 957, 597, 477, 354, 677, 691, 25, 580, 897, 542, 186, 359, 346, 409, 655, 979, 853, 411, 344, 358, 559, 765, 383, 484, 181, 82, 514, 582, 593, 77, 228, 921, 348, 453, 274, 449, 106, 657, 783, 782, 811, 333, 305, 784, 581, 746, 858, 249, 479, 652, 270, 429, 614, 903, 102, 378, 575, 119, 196, 12, 990, 356, 277, 169, 70, 518, 282, 676, 137, 622, 616, 357, 913, 161, 3, 589, 327));
-    }
 
     @Test
     public void diagonal() {
@@ -387,36 +330,6 @@ public class CrackCodeString {
         }
 
         return  result;
-    }
-
-    @Test
-    public void generate() {
-        System.out.println(generate(5));
-    }
-
-
-    public ArrayList<ArrayList<Integer>> generate(int a) {
-
-        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
-        result.add(new ArrayList<Integer>(Arrays.asList(1)));
-        for (int i = 1; i < a; i++) {
-            int prev= result.size()-1;
-
-
-            if(a==0)
-                return result;
-
-            ArrayList<Integer> row = new ArrayList<>();
-            row.add(1);
-            for (int j = 0; j < result.get(prev).size()-1; j++) {
-                int sum = result.get(prev).get(j) + result.get(prev).get(j + 1);
-                row.add(sum);
-            }
-            row.add(1);
-            result.add(row);
-        }
-
-        return result;
     }
 
     @Test
@@ -1109,12 +1022,12 @@ public class CrackCodeString {
         );
 
 
-        a = Arrays.asList(
+        /*a = Arrays.asList(
                 Arrays.asList(1),
                 Arrays.asList(5),
                 Arrays.asList(9),
                 Arrays.asList(13)
-        );
+        );*/
 /*
         a = Arrays.asList(
                 Arrays.asList(1,2)
