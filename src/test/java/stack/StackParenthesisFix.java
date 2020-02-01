c82a10feb55cdd1db6b0989bcf5275c738e6e81c
john lim <johnlim@johns-MacBook-Pro-2.local>
2020-02-01 14:57:33 +0800

    stack paren fix

---
+++ src/test/java/stack/StackParenthesisFix.java
@@ -0,0 +1,25 @@
+package stack;
+
+/**
+ * how many count needed to fix the parenthesis
+ */
+public class StackParenthesisFix {
+    static int bracketMatch(String text) {
+        // your code goes here
+        int count=0;
+        int fix = 0;
+        for(int i =0; i < text.length(); i++){
+            if(text.charAt(i) == '(') count++;
+            else count--;
+            if(count < 0) {
+                fix++;
+                count=0;
+            }
+        }
+        return fix + count; // remaining open
+    }
+
+    public static void main(String[] args) {
+        System.out.println(bracketMatch("())("));
+    }
+}
