311c661c1ecd3c1c4024e50744ced289ad599c1b
john lim <johnlim@johns-MacBook-Pro-2.local>
2020-02-03 23:16:04 +0800

    missing positive non negative

--- src/test/java/sort/MissingPositive.java
+++ src/test/java/sort/MissingPositive.java
@@ -18,6 +18,26 @@ public class MissingPositive {
         return min;
     }
 
+    /**
+     *
+     * the missing positive should be within the array
+     */
+    public int firstMissingPositiveNonNegative(int[] nums) {
+        for(int i= 0; i < nums.length; i++){
+            int temp = nums[i];
+            // 3 1, 2, 0
+            if(temp!=i && temp < nums.length){
+                int t2 =  nums[temp];
+                nums[temp] = nums[i];
+                nums[i] = t2;
+                i--; // backtrack
+            }
+        }
+        for(int i= 0; i <nums.length ; i++) {
+            if(i!=nums[i]) return i;
+        }
+        return nums.length;
+    }
     public static void main(String[] args) {
         MissingPositive mp  = new MissingPositive();
         out.println(mp.firstMissingPositive(new int[]{2,3,3, 6}));
@@ -25,5 +45,6 @@ public class MissingPositive {
         out.println(mp.firstMissingPositive(new int[]{-1,-3,-3,2, 6}));
         out.println(mp.firstMissingPositive(new int[]{-1,-3}));
         out.println(mp.firstMissingPositive(new int[]{6}));
+        out.println(mp.firstMissingPositiveNonNegative(new int[]{600,100,60,5}));
     }
 }
