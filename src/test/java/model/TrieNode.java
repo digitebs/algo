dae61a8a225540fb8fc7b3ef065f7df70ac7265e
john lim <johnlim@johns-MacBook-Pro-2.local>
2020-02-04 22:30:11 +0800

    adding tries datastructure

---
+++ src/test/java/model/TrieNode.java
@@ -0,0 +1,9 @@
+package model;
+
+import java.util.HashMap;
+
+public class TrieNode{
+    public String content; //dictionaries
+    public HashMap<Character,TrieNode> children= new HashMap<>();
+    public boolean isEndOfWord;
+}
