d8f6b2502763e13ffdbaff0fa40a4929af390cb2
Lim, John <john.lim@rakuten.com>
2020-02-05 12:10:00 +0800

    no divide

--- src/test/java/math/ProductExceptSelf.java
+++ src/test/java/math/ProductExceptSelf.java
@@ -3,16 +3,19 @@ package math;
 import java.util.Arrays;
 
 public class ProductExceptSelf {
-    static int[] productExceptSelf(int[] num) {
+    /*
+        No divide
+     */
+    static int[] productExceptSelf(int[] nums) {
         // 1 2 3 4
         // (2 * 3 * 4)
         // (1) * (3 * 4)
         // (1 * 2) * (4)
         // (1 * 2 * 3)
-        int[] dp = new int[num.length];
+        int[] dp = new int[nums.length];
         dp[0] = 1;
-        for (int i = 1, tmp = 1; i < num.length; i++) dp[i] = tmp *= num[i - 1];
-        for (int i = num.length - 2, tmp = 1; i >= 0; i--) dp[i] *= tmp *= num[i + 1];
+        for (int i = 1, tmp = 1; i < nums.length; i++) dp[i] = tmp *= nums[i - 1]; // 1, 1*2, 1*2,*3 1st half
+        for (int i = nums.length - 2, tmp = 1; i >= 0; i--) dp[i] *= tmp *= nums[i + 1]; // 4, 4*3, 4*3*2 lower half
         return dp;
     }
 
