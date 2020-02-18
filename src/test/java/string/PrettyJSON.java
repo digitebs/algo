5d38e6d0f2a8024c804e23abd72b7f04219db6c4
iody <ebs_cbn@yahoo.com>
2020-02-18 23:35:04 +0800

    number to words

--- src/test/java/string/PrettyJSON.java
+++ src/test/java/string/PrettyJSON.java
@@ -2,6 +2,7 @@ package string;
 
 import java.util.ArrayList;
 
+/** @author johnlim */
 public class PrettyJSON {
   static ArrayList<String> prettyJSON(String a) {
     ArrayList<String> res = new ArrayList<>();
@@ -12,12 +13,13 @@ public class PrettyJSON {
       char c = a.charAt(i);
 
       if (c == '[' || c == '{') { // next line tabcount+1
-        if (sb.length() == 0) res.add(tabStr + sb);
-        res.add(tabStr + c);
-        sb.setLength(0);
+        if (sb.length() != 0) res.add(tabStr + sb);
         tabStr += "\t";
+        res.add(tabStr + c);
+        tabStr += "\t";
+        sb.setLength(0);
       } else if (c == ']' || c == '}') {
-        if (sb.length() == 0) res.add(tabStr + sb);
+        if (sb.length() != 0) res.add(tabStr + sb);
         res.add((tabStr = tabStr.substring(0, tabStr.length() - 1)) + c);
         sb.setLength(0);
       } else if (c == ',') {
@@ -25,14 +27,15 @@ public class PrettyJSON {
           res.set(res.size() - 1, res.get(res.size() - 1) + c);
         else res.add(tabStr + sb + c);
         sb.setLength(0);
-      } else sb.append(c);
+      } else if (c != ' ' || sb.length() != 0) sb.append(c);
     }
 
     return res;
   }
 
   public static void main(String[] args) {
-    ArrayList<String> lines = prettyJSON("[\"foo\", {\"bar\":[\"baz\",null,1.0,2]}]");
+    ArrayList<String> lines =
+        prettyJSON("[\"foo\", {\"bar\":[\"name\":\"baz\",null,1.0,2,\"hello world\"]}]");
     lines.forEach(System.out::println);
   }
 }
