945026ef946ac790277ba74a7a25f02601bae601
Lim, John <john.lim@rakuten.com>
2020-02-19 12:32:01 +0800

    Merge branch 'master' of https://github.com/iody/algo
    # Conflicts:
    #	src/test/java/string/ReverseWords.java

--- src/test/java/string/PrettyJSON.java
+++ src/test/java/string/PrettyJSON.java
@@ -1,11 +1,12 @@
 package string;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /** @author johnlim */
 public class PrettyJSON {
-  static ArrayList<String> prettyJSON(String a) {
-    ArrayList<String> res = new ArrayList<>();
+  static List<String> prettyJSON(String a) {
+    List<String> res = new ArrayList<>();
 
     String tabStr = "";
     StringBuilder sb = new StringBuilder();
@@ -13,14 +14,13 @@ public class PrettyJSON {
       char c = a.charAt(i);
 
       if (c == '[' || c == '{') { // next line tabcount+1
-        if (sb.length() != 0) res.add(tabStr + sb);
-        tabStr += "\t";
-        res.add(tabStr + c);
+        res.add(tabStr + sb + c);
         tabStr += "\t";
         sb.setLength(0);
       } else if (c == ']' || c == '}') {
         if (sb.length() != 0) res.add(tabStr + sb);
         res.add((tabStr = tabStr.substring(0, tabStr.length() - 1)) + c);
+        //tabStr = tabStr.substring(0, tabStr.length()-1);
         sb.setLength(0);
       } else if (c == ',') {
         if (a.charAt(i - 1) == ']' || a.charAt(i - 1) == '}')
@@ -34,8 +34,8 @@ public class PrettyJSON {
   }
 
   public static void main(String[] args) {
-    ArrayList<String> lines =
-        prettyJSON("[\"foo\", {\"bar\":[\"name\":\"baz\",null,1.0,2,\"hello world\"]}]");
+    List<String> lines =
+        prettyJSON("[\"foo\": {\"bar\": [\"name\":\"baz\",null,1.0,2,\"hello:world\"]}]");
     lines.forEach(System.out::println);
   }
 }
