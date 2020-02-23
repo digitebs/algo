46c11b01b86ed4ee82a8b22fd5ceb008501ba80e
iody <ebs_cbn@yahoo.com>
2020-02-23 10:49:33 +0800

    added data

--- src/test/java/interval/BusiestPeriod.java
+++ src/test/java/interval/BusiestPeriod.java
@@ -24,41 +24,59 @@ input:  data = [ [1487799425, 14, 1],
 output: 1487800378
  */
 public class BusiestPeriod {
-    static int findBusiestPeriod(int[][] a) {
-        // your code goes here
-        int max=0;
-        int res=0;
-        int num=0;
+  static int findBusiestPeriod(int[][] a) {
+    // your code goes here
+    int max = 0;
+    int res = 0;
+    int num = 0;
 
-        int people = a[0][1];
-        if(a[0][2] == 1) num+=people;
-        else num-=people;
+    int people = a[0][1];
+    if (a[0][2] == 1) num += people;
+    else num -= people;
 
-
-        for( int i=1; i < a.length;i++){
-            if( a[i][0] == a[i-1][0]){
-                people = a[i][1];
-                if(a[i][2] == 1) num+=people;
-                else num-=people; // 18
-
-            }else if(a[i][0] != a[i-1][0]){
-                if(max < num){
-                    max = num;
-                    res = a[i-1][0]; //8
-                }
-
-                people = a[i][1];
-                if(a[i][2] == 1)
-                    num+=people; // + 10
-                else
-                    num-=people;
-            }
+    for (int i = 1; i < a.length; i++) {
+      // if( a[i][0] == a[i-1][0]){
+      //    }
+      if (a[i][0] != a[i - 1][0]) {
+        if (max < num) {
+          max = num;
+          res = a[i - 1][0]; // 8
         }
-        if(max < num){
-            max = num;
-            res = a[a.length-1][0];
-        }
-        // 1487901211, 7,  0
-        return res;
+      }
+      people = a[i][1];
+      if (a[i][2] == 1) num += people; // + 10
+      else num -= people;
     }
+    if (max < num) {
+      max = num;
+      res = a[a.length - 1][0];
+    }
+    // 1487901211, 7,  0
+    return res;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(
+        findBusiestPeriod(
+            new int[][] {
+              {1487799425, 14, 1},
+              {1487799425, 4, 0},
+              {1487799425, 2, 0},
+              {1487800378, 10, 1},
+              {1487801478, 18, 0},
+              {1487801478, 18, 1},
+              {1487901013, 1, 0},
+              {1487901211, 7, 1},
+              {1487901211, 7, 0}
+            }));
+
+    System.out.println(findBusiestPeriod(new int[][] {{1487799426, 21, 1}}));
+    System.out.println(
+        findBusiestPeriod(
+            new int[][] {
+              {1487799425, 21, 0},
+              {1487799427, 22, 1},
+              {1487901318, 7, 0}
+            }));
+  }
 }
