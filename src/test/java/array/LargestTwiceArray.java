44e24c5f9c184cba95656a98f164e366c0768ef0
iody <ebs_cbn@yahoo.com>
2020-02-23 11:15:38 +0800

    adding comments

--- src/test/java/array/LargestTwiceArray.java
+++ src/test/java/array/LargestTwiceArray.java
@@ -1,5 +1,18 @@
 package array;
 
+/*
+  Input : arr = {3, 6, 1, 0}
+  Output : 1
+  Here, 6 is the largest integer, and for
+  every other number in the array x, 6 is
+  more than twice as big as x. The index of
+  value 6 is 1, so we return 1.
+
+  Input : arr = {1, 2, 3, 4}
+  Output : -1
+  4 isn't at least as big as twice the value
+  of 3, so we return -1.
+ */
 public class LargestTwiceArray {
   public int largestTwice(int[] nums) {
     int index = 0;
@@ -10,5 +23,6 @@ public class LargestTwiceArray {
 
   public static void main(String[] args) {
     System.out.println(new LargestTwiceArray().largestTwice(new int[] {3, 6, 1, 0}));
+    System.out.println(new LargestTwiceArray().largestTwice(new int[] {1, 2, 3, 4}));
   }
 }
