9403f2dc7cf0c409df2e722b7237049a19961ce0
iody <ebs_cbn@yahoo.com>
2020-02-28 22:29:08 +0800

    adding more description

--- src/test/java/matrix/ArrayRotate902d.java
+++ src/test/java/matrix/ArrayRotate902d.java
@@ -2,6 +2,10 @@ package matrix;
 
 import java.util.Arrays;
 
+/*
+  You are given an n x n 2D matrix that represents an image.
+
+ */
 public class ArrayRotate902d {
 
   // 1 2 3 4
@@ -15,27 +19,27 @@ public class ArrayRotate902d {
   // 16 12 8 4
 
   // can be done by layer inplace
-  static int[][] rotate(int[][] arr) {
-    int n = arr.length;
-    for (int i = 0; i < n / 2; i++) { // layer
-      for (int j = i; j < n - 1 - i; j++) { // next
-        int temp = arr[i][j];
-        arr[i][j] = arr[n - 1 - j][i];
-        arr[n - 1 - j][i] = arr[n - 1 - i][n - 1 - j];
-        arr[n - 1 - i][n - 1 - j] = arr[j][n - 1 - i];
-        arr[j][n - 1 - i] = temp;
+  static int[][] rotate(int[][] a) {
+    int n = a.length - 1;
+    for (int i = 0; i <= n / 2; i++) {
+      for (int j = i; j < n - i; j++) {
+        int temp = a[i][j];
+        a[i][j] = a[n - j][i];
+        a[n - j][i] = a[n - i][n - j];
+        a[n - i][n - j] = a[j][n - i];
+        a[j][n - i] = temp;
       }
     }
-    return arr;
+    return a;
   }
 
   /* using new array */
-  private int[][] rotateMatrix(int[][] src) {
-    int[][] img = new int[src.length][src.length];
-
-    for (int i = 0; i < src.length; i++) {
-      for (int j = 0; j < src.length; j++) {
-        img[i][j] = src[src.length - 1 - j][i];
+  static int[][] rotateMatrix(int[][] a) {
+    int[][] img = new int[a.length][a.length];
+    int n = a.length - 1;
+    for (int i = 0; i <= n; i++) {
+      for (int j = 0; j <= n; j++) {
+        img[i][j] = a[n - j][i];
         System.out.print(img[i][j] + " ");
       }
       System.out.print("\n");
@@ -47,7 +51,7 @@ public class ArrayRotate902d {
     int[][] arr = new int[][] {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
     System.out.println(Arrays.deepToString(rotate(arr)));
     arr = new int[][] {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    System.out.println(Arrays.deepToString(rotate(arr)));
+    System.out.println(Arrays.deepToString(rotateMatrix(arr)));
     arr = new int[][] {{1, 2}, {3, 4}};
     System.out.println(Arrays.deepToString(rotate(arr)));
     arr = new int[][] {{1}};
