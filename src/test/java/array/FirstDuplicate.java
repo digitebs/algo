d22e3b03544f293a4b7972546e06a3a9bbe9502d
iody <ebs_cbn@yahoo.com>
2020-02-28 21:49:44 +0800

    adding more description

---
+++ src/test/java/array/FirstDuplicate.java
@@ -0,0 +1,32 @@
+package array;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+/*
+Given an array a that contains only numbers in the range from 1 to a.length,
+ find the first duplicate number for which the second occurrence has the minimal index.
+  In other words, if there are more than 1 duplicated numbers,
+   return the number for which the second occurrence has a smaller index
+   than the second occurrence of the other number does. If there are no such elements, return -1.
+
+   its just means return the 1st duplicate you find by iterating
+ */
+public class FirstDuplicate {
+  public int firstDuplicate(final int[] a) {
+    int[] index = new int[a.length + 1];
+    for (int i = 0; i < a.length; i++) {
+      if (index[a[i]] == 1) return a[i];
+      index[a[i]] = 1;
+    }
+    return -1;
+  }
+
+  public static void main(String[] args) {
+    //
+    FirstDuplicate fd = new FirstDuplicate();
+    System.out.print(fd.firstDuplicate(new int[]{3, 4, 1, 4, 1}));
+  }
+}
---
+++ src/test/java/array/FirstNotRepeatingCharacter.java
@@ -0,0 +1,27 @@
+package array;
+
+
+/*
+Given a string s consisting of small English letters,
+ find and return the first instance of a non-repeating character in it.
+  If there is no such character, return '_'.
+ */
+public class FirstNotRepeatingCharacter {
+  char firstNotRepeatingCharacter(String s) {
+    int[] count = new int[26];
+    for(int i = 0; i < s.length(); i++){
+      count[s.charAt(i) - 'a']++;
+    }
+    for(int i = 0; i < s.length(); i++){
+      if(count[s.charAt(i) - 'a'] == 1)
+        return s.charAt(i);
+    }
+    return '_';
+  }
+
+  public static void main(String[] args) {
+    //
+    FirstNotRepeatingCharacter fd = new FirstNotRepeatingCharacter();
+    System.out.print(fd.firstNotRepeatingCharacter("abacabad"));
+  }
+}
