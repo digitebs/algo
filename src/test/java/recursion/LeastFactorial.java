253cc7ebb5396e8e6dd0937c73b8b0d8d63e8036
iody <ebs_cbn@yahoo.com>
2020-03-02 05:18:52 +0800

    adding description

---
+++ src/test/java/recursion/LeastFactorial.java
@@ -0,0 +1,22 @@
+package recursion;
+
+/*
+    easy
+ */
+public class LeastFactorial {
+    int leastFactorial(int n) {
+        int res = 0;
+        for(int i = 1;(res = factorial(i)) < n;i++);
+        return res;
+    }
+
+    int factorial( int n ){
+        if(n == 0) return 1;
+        return n * factorial(n-1);
+    }
+
+  public static void main(String[] args) {
+        LeastFactorial lf = new LeastFactorial();
+    System.out.println(lf.leastFactorial(24));
+  }
+}
