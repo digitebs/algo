30928bb2cbeedee1d15120a551fd218688257867
Lim, John <john.lim@rakuten.com>
2020-03-02 15:57:42 +0800

    update msg

--- src/test/java/map/CountTriplets.java
+++ src/test/java/map/CountTriplets.java
@@ -9,12 +9,13 @@ import java.util.List;
 
 medium*
 
-You are given an array and you need to find number of tripets of indices
+You are given a sorted array and you need to find number of tripets of indices
   such that the elements at those indices are in geometric progression for a given common ratio r and i <j <k .
 
 {1,4,16,64} r = 4
 {1,4,16}
 {4,16,64}
+
 
 2
 
@@ -39,5 +40,10 @@ public class CountTriplets {
 
   public static void main(String[] args) {
     System.out.println(countTriplets(Arrays.asList(5L, 10L, 20L), 2));
+    System.out.println(countTriplets(Arrays.asList(1L, 5L, 5L,25L), 5));
+    System.out.println(countTriplets(Arrays.asList(1L,4L,16L, 64L), 4));
+    System.out.println(countTriplets(Arrays.asList(1L, 3L, 9L, 9L, 27L, 81L), 3));
+    System.out.println(countTriplets(Arrays.asList(1L, 1L, 1L, 1L, 1L, 1L,1L,1L,1L,1L,1L), 1));
+
   }
 }
