7b3149139327943ca8129234fccac814f0a6654d
iody <ebs_cbn@yahoo.com>
2020-03-23 03:25:42 +0800

    largest rectangle

--- src/test/java/interval/MergeInterval.java
+++ src/test/java/interval/MergeInterval.java
@@ -1,9 +1,6 @@
 package interval;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class MergeInterval {
   // merge non overlapping intervals
@@ -11,14 +8,16 @@ public class MergeInterval {
     Collections.sort(
         list,
         (o1, o2) -> {
-          int diff = o1.get(0) - o2.get(0);
-          return diff;
+          return o1.get(0) - o2.get(0);
         });
 
-    for (int i = list.size() - 1; i > 0; i--) {
-      if (list.get(i - 1).get(1) >= list.get(i).get(0)) {
-        list.get(i - 1).set(1, list.get(i).get(1));
-        list.remove(i); // remove this;
+    for(int i = 0; i < list.size()-1;i++){
+      List<Integer> curr = list.get(i);
+      List<Integer> next = list.get(i+1);
+      if (curr.get(1) >= next.get(0)) {
+        curr.set(1, Math.max(next.get(1),curr.get(1)));
+        list.remove(i+1);
+        i--;
       }
     }
 
@@ -29,7 +28,7 @@ public class MergeInterval {
   public static void main(String[] args) {
     List<List<Integer>> list = new ArrayList<>();
     list.add(Arrays.asList(1, 2));
-    list.add(Arrays.asList(1, 3));
+    list.add(Arrays.asList(1, 8));
     list.add(Arrays.asList(1, 4));
     list.add(Arrays.asList(4, 5));
     list.add(Arrays.asList(6, 8));
