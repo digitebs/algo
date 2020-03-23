dc9056efbae8378300ba6dcdfa0494ef9027960e
iody <ebs_cbn@yahoo.com>
2020-03-24 06:43:17 +0800

    rename var

--- src/test/java/greedy/ProductExceptSelf.java
+++ src/test/java/greedy/ProductExceptSelf.java
@@ -14,14 +14,14 @@ public class ProductExceptSelf {
     // (1 * 2 * 3)
     int n = nums.length;
     int[] dp = new int[n];
-    if(n <= 1) return new int[]{};
-    for (int i = 0, product = 1; i < n; i++) {
-      dp[i] = product;
-      product *= nums[i]; // 1, 1*2, 1*2,*3 1st half
+    if (n <= 1) return new int[] {};
+    for (int i = 0, p = 1; i < n; p *= nums[i], i++) {
+      dp[i] = p;
+      // product *= nums[i]; // 1, 1*2, 1*2,*3 1st half
     }
-    for (int i = n - 1, product = 1; i >= 0; i--){
-      dp[i] *= product;
-      product *= nums[i]; // 4, 4*3, 4*3*2 lower half
+    for (int i = n - 1, p = 1; i >= 0; p *= nums[i], i--) {
+      dp[i] *= p;
+      // 4, 4*3, 4*3*2 lower half
     }
     return dp;
   }
