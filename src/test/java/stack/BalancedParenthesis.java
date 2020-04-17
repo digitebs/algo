3f2b290cca7513b82da8c6d4cd0a0a91312e23e0
iody <ebs_cbn@yahoo.com>
2020-04-18 07:44:48 +0800

    isb

--- src/test/java/stack/BalancedParenthesis.java
+++ src/test/java/stack/BalancedParenthesis.java
@@ -14,7 +14,8 @@ import java.util.Stack;
     Output: YES
  */
 public class BalancedParenthesis {
-   String isBalanced(String s) {
+
+  String isBalanced(String s) {
     Stack<Character> stack = new Stack<>();
     for (int i = 0; i < s.length(); i++) {
       char c = s.charAt(i);
@@ -28,9 +29,31 @@ public class BalancedParenthesis {
     return stack.isEmpty() ? "YES" : "NO";
   }
 
+
+  /*
+    : can be either (, ) or empty
+   */
+  boolean isBalanced2(String s, int b) {
+
+    for (int i = 0; i < s.length(); i++) {
+      if (s.charAt(i) == ':') {
+        final String substring = s.substring(i + 1, s.length());
+        return isBalanced2('(' + substring, b)
+            || isBalanced2(')' + substring, b) ||
+            isBalanced2(substring, b);
+      } else if (s.charAt(i) == '(') b++;
+      else if(b > 0) b--;
+      else return false;
+    }
+    return b == 0;
+  }
+
   public static void main(String[] args) {
     BalancedParenthesis bp = new BalancedParenthesis();
     System.out.println(bp.isBalanced("{[]}"));
     System.out.println(bp.isBalanced("{[()[()]]([])}"));
+    System.out.println(bp.isBalanced2("()",0));
+    System.out.println(bp.isBalanced2("(:)",0));
+    System.out.println(bp.isBalanced2("(:))",0));
   }
 }
