c6b162f04e8b464591ebe92f72af197fd9eba680
Lim, John <john.lim@rakuten.com>
2020-05-02 07:53:45 +0800

    fixing binary

--- src/test/java/string/AddBaseString.java
+++ src/test/java/string/AddBaseString.java
@@ -8,7 +8,8 @@ Input:
 
 Output: "100"
  */
-public class AddBinaryString {
+public class AddBaseString {
+  static int BASE = 3;
   static String addBinary(String a, String b) {
     a = String.format("%" + b.length() + "s", a).replace(' ', '0');
     b = String.format("%" + a.length() + "s", b).replace(' ', '0');
@@ -16,11 +17,11 @@ public class AddBinaryString {
     String ans = "";
     int c = 0;
     for (int i = a.length() - 1; i >= 0; i--) {
-      int x = Character.digit(a.charAt(i), 2);
-      int y = Character.digit(b.charAt(i), 2);
+      int x = Character.digit(a.charAt(i), BASE);
+      int y = Character.digit(b.charAt(i), BASE);
       int sum = x + y + c;
-      c = sum / 2;
-      int r = sum % 2;
+      c = sum / BASE;
+      int r = sum % BASE;
       ans = r + ans;
     }
     return (c == 1 ? c : "") + ans;
@@ -28,7 +29,8 @@ public class AddBinaryString {
 
   public static void main(String[] args) {
 
-    System.out.println(addBinary("1", "10001"));
+    System.out.println(addBinary("1", "10002"));
+    System.out.println(addBinary("12", "2"));
     System.out.println(addBinary("1100101010010", "01010100101001010"));
   }
 }
