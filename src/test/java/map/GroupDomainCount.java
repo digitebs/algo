d38022735959ee0530f6e38e0d455a68ce86b67b
Lim, John <john.lim@rakuten.com>
2020-07-29 12:38:04 +0800

    fix d count

--- src/test/java/map/GroupDomainCount.java
+++ src/test/java/map/GroupDomainCount.java
@@ -2,7 +2,32 @@ package map;
 
 import java.util.HashMap;
 
-/** return click count seperated by domain */
+/**
+ given a string list of "click count , domain"
+
+ return the sum of click count by domain,subdomain, 1st level, 2nd level etc...
+
+ Input:
+ [
+ "900,google.com",
+ "60,mail.yahoo.com",
+ "10,mobile.sports.yahoo.com",
+ "40,sports.yahoo.com",
+ "300,yahoo.com",
+ "10,stackoverflow.com",
+ "20,overflow.com",
+ "2,en.wikipedia.org",
+ "1,m.wikipedia.org",
+ "1,mobile.sports",
+ "1,rakuten.co.jp" ,
+ "1,google.co.jp"
+ ]
+
+
+ Output:
+ {com=1340, stackoverflow.com=10, sports.yahoo.com=50, google.com=900, sports=1, org=3, rakuten.co.jp=1, wikipedia.org=3, jp=2, mobile.sports=1, co.jp=2, google.co.jp=1, mail.yahoo.com=60, en.wikipedia.org=2, mobile.sports.yahoo.com=10, yahoo.com=410, m.wikipedia.org=1, overflow.com=20}
+
+ */
 public class GroupDomainCount {
   static void test(String[] counts) {
     HashMap<String, Integer> hm = new HashMap<>();
@@ -35,7 +60,8 @@ public class GroupDomainCount {
       "2,en.wikipedia.org",
       "1,m.wikipedia.org",
       "1,mobile.sports",
-      "1,google.co.uk"
+      "1,rakuten.co.jp",
+      "1,google.co.jp"
     };
 
     test(counts);
