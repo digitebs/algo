0151a53fbbc090b5d8a4a6321e26385e10295103
iody <ebs_cbn@yahoo.com>
2021-06-26 15:11:42 +0800

    fix sqrt

--- src/test/java/search/Sqrt.java
+++ src/test/java/search/Sqrt.java
@@ -54,9 +54,32 @@ public class Sqrt {
   //  return nums.get(binarySearch(nums, x)).intValue();
   }
 
+  // epsillon
+  double binarySearch(double target) {
+    double lo = 0;
+    double hi = target;
+    double eps = 0.001;
+
+    // 5 9 11 12 13 14 20 25
+    while (lo < hi) {
+      double mid = ( hi + lo) / 2.0;
+      double num = mid*mid;
+      //System.out.println(hi +" "+lo+" "+ mid);
+      if (target - num >= 0 && target - num <= eps) {
+          return mid;
+      }else if(num > target){
+        hi = mid;
+      } else {
+        lo = mid;
+      }
+      //  System.out.println(right+ " "+mid+" "+left);
+    }
+    return -1;
+  }
   public static void main(String[] args) {
     Sqrt sq = new Sqrt();
-    System.out.println(sq.mySqrt(5));
-    System.out.println(sq.binarySearch2(16));
+    //System.out.println(sq.mySqrt(5));
+    //System.out.println(sq.binarySearch2(16));
+    System.out.println(sq.binarySearch(9));
   }
 }
