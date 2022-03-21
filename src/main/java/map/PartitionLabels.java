750161ea2e5712074ab8db9bf9128ce570c9bfe5
iody <ebs_cbn@yahoo.com>
2022-03-21 09:19:36 +0800

    partition labels

---
+++ src/main/java/map/PartitionLabels.java
@@ -0,0 +1,52 @@
+package map;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * You are given a string s. We want to partition the string into as many parts as possible so that each letter appears in at most one part.
+ *
+ * Note that the partition is done so that after concatenating all the parts in order, the resultant string should be s.
+ *
+ * Return a list of integers representing the size of these parts.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "ababcbacadefegdehijhklij"
+ * Output: [9,7,8]
+ * Explanation:
+ * The partition is "ababcbaca", "defegde", "hijhklij".
+ * This is a partition so that each letter appears in at most one part.
+ * A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits s into less parts.
+ * Example 2:
+ *
+ * Input: s = "eccbbbbdec"
+ * Output: [10]
+ *
+ */
+public class PartitionLabels {
+  public List<Integer> partitionLabels(String s) {
+    int[] last = new int[26];
+    for(int i=0; i < s.length();i++)
+      last[s.charAt(i) -'a'] = i;
+
+    var res = new ArrayList<Integer>();
+    int j =0 , start=-1;
+    for(int i=0; i < s.length();i++){
+      j = Math.max(last[s.charAt(i)-'a'],j);
+      if(j == i){
+        res.add(j - start);
+        start=i;
+      }
+    }
+    return res;
+  }
+
+  public static void main(String[] args) {
+    var pl = new PartitionLabels();
+    System.out.println(pl.partitionLabels("ababcbacadefegdehijhklij"));
+    System.out.println(pl.partitionLabels("eccbbbbdec"));
+  }
+}
